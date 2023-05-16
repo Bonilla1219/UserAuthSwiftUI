d50b33b2d8f16436b2025ad4a3162907c1bee650
Javierb1219 <bonillaculture1219@gmail.com>
2023-05-15 23:49:17 -0700

    Initial Commit

---
+++ UserAuthSwiftUI/UserAuthSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAuthSwiftUIApp.swift
+//  UserAuthSwiftUI
+//
+//  Created by Javier Bonilla on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct UserAuthSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
